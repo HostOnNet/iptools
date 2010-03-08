36f0528cd639936cb95b3c341ba97807973af4f8
HostOnNet.com <admin@hostonnet.com>
2010-03-09 03:25:33 +0530

    format

--- skype.php
+++ skype.php
@@ -7,27 +7,27 @@ This program is to read chat log of skype with opera speak
 if (isset($_POST['body']))
 {
     $quotes = get_magic_quotes_gpc();
-
-	$text = $_POST['body'];
+    
+    $text = $_POST['body'];
     
     if ($quotes)
     {
         $text = stripslashes($text);
     }
-
+    
     $lines = explode("\n", $text);
     echo '<pre>';
     
     foreach ($lines as $line)
     {
-		$line = preg_replace('/\[[^\]]+\]/i', '', $line);
-		$line = wordwrap($line, 80, "\n");
-    	echo $line . "\n";
+        $line = preg_replace('/\[[^\]]+\]/i', '', $line);
+        $line = wordwrap($line, 80, "\n");
+        echo $line . "\n";
     }
     
-	echo '</pre>';
+    echo '</pre>';
     echo '<br /><br /><a href="/post.php">Back</a>';
-    
+
 }
 else
 {
